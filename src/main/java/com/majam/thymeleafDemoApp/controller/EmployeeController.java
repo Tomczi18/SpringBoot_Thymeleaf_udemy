f0392966ffa567240b8a200404124b329a076b52
Tom_i <majamex123@gmail.com>
2022-08-31 15:17:22 +0200

    adding to database

--- src/main/java/com/majam/thymeleafDemoApp/controller/EmployeeController.java
+++ src/main/java/com/majam/thymeleafDemoApp/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.majam.thymeleafDemoApp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -57,7 +54,30 @@ public class EmployeeController {
         return "redirect:/employees/list";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId,
+                                    Model theModel) {
 
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee as a model attribute to pre-populate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to /employees/list
+        return "redirect:/employees/list";
+
+    }
 }
 
 
