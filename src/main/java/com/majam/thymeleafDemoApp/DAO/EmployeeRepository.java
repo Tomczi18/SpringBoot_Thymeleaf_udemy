bbdb40dae4a08e6b0b52e07e0b830bafe728b95a
Tom_i <majamex123@gmail.com>
2022-08-31 14:34:37 +0200

    adding to database

--- src/main/java/com/majam/thymeleafDemoApp/DAO/EmployeeRepository.java
+++ src/main/java/com/majam/thymeleafDemoApp/DAO/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.majam.thymeleafDemoApp.DAO;
 import com.majam.thymeleafDemoApp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
 }
